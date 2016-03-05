f37dfa89a5fa2fcf980ffa356269bb11ed37212d
Simona Petrova <simona.nicolaeva.petrova@abv.bg>
2016-03-06 00:22:13 +0200

    no message

---
+++ src/Library.java
@@ -0,0 +1,48 @@
+import java.awt.List;
+import java.util.ArrayList;
+
+public class Library extends ArrayList {
+	String name;
+
+	public void add(Book book) {
+
+	}
+
+	String[][] List;
+
+	// Collections.addAll(List, book);
+
+	public Library() {
+
+	}
+
+	// public void print (){
+	// for (int i=0;i<n;i++){
+	// for(int j=0;j<3;j++){
+	// System.out.print(list[i][j] + " ");
+	// }
+	// System.out.println();
+	// }
+	// }
+	//
+	// public void searchByAuthor(String author) {
+	// for (int i = 0; i < n; i++) {
+	// if (list[i][1].equals(author)) {
+	// for (int j = 0; j < 4; j++) {
+	// System.out.print(list[i][j] + " ");
+	// }
+	// }
+	// }
+	//
+	//
+	// }
+	// public void searchBook(String title) {
+	// for (int i = 0; i < n; i++) {
+	// if (list[i][0].equals(title)) {
+	// for (int j = 0; j < 4; j++) {
+	// System.out.print(list[i][j] + " ");
+	// }
+	// }
+	// }
+
+}
---
+++ src/test.java
@@ -0,0 +1,14 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		Library HristoBotev =new Library();
+		Book book1 = new Book("Happy", "Az", "Helikon", "24654165465");
+		Book book2 = new Book("Happy", "Az", "Helikon", "24654165465");
+		HristoBotev.add(book1);
+		HristoBotev.add(book2);
+		
+
+	}
+
+}
